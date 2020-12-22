f9d336c045a7ca68d6b892440fef3e9bf3e0f23b
Sei Sebastian <18251746+Seiseigy@users.noreply.github.com>
2020-12-22 00:16:36 -0300

    Agregada selección de indicador, arreglo de indicadores hardcodeado

--- paisesIndicadores/src/pages/IndicatorSelector.vue
+++ paisesIndicadores/src/pages/IndicatorSelector.vue
@@ -18,11 +18,19 @@
             </ion-item>
             <ion-item>
                 <ion-label>Segundo país</ion-label>
-                <ion-select v-model="firstCountrySelected" @ionChange="firstCountrySelected= $event.target.value" v-on:click="updateCountriesList()">
+                <ion-select v-model="secondCountrySelected" @ionChange="secondCountrySelected= $event.target.value" v-on:click="updateCountriesList()">
                         <ion-select-option v-for="country in hiddenCountries" v-bind:key="country.code" v-bind:value="country.name">{{country.name}}</ion-select-option>
                 </ion-select>
             </ion-item>
-            <span>Seleccionado: {{firstCountrySelected}}</span>
+            <ion-item>
+                <ion-label>Indicador</ion-label>
+                <ion-select v-model="indicatorSelected" @ionChange="indicatorSelected= $event.target.value">
+                        <ion-select-option v-for="indicator in indicators" v-bind:key="indicator[1]" v-bind:value="indicator[0]">{{indicator[0]}}</ion-select-option>
+                </ion-select>
+            </ion-item>
+            <span><br> Primer país: {{firstCountrySelected}} <br></span>
+            <span><br> Segundo país: {{secondCountrySelected}}<br></span>
+            <span><br> Indicador: {{indicatorSelected}}<br></span>
         </ion-content>
     </ion-page>
 </template>
@@ -40,9 +48,19 @@ export default {
     data () {
         return {
             countries: [],
-            indicators: [],
+            indicators: [
+                ['Producto Interno Bruto','PIB'],
+                ['Tasa de desempleo anual','TDA'],
+                ['Inflación','IFL'],
+                ['Impuesto de Valor Añadido','IVA'],
+                ["Presión Fiscal","PRF"],
+                ["Salario Mínimo", "SMI"],
+                [ "Tasa de cambio","TSC"],
+                ["Doing Business Index", "DBI"],            
+            ],
             firstCountrySelected: '',
             secondCountrySelected: '',
+            indicatorSelected: '',
             hiddenCountries: []
         }
     },
